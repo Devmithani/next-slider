9c9934eaa6a996e016c4dc11c86f2b630fe2120b
mithani <m.mithani009@gmail.com>
2024-07-10 06:18:53 +0500

    work uploaded

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,10 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {
+    "/": [
+      "static/media/f769c49a4b8c1350-s.p.woff2"
+    ]
+  },
+  "app": {},
+  "appUsingSizeAdjust": false,
+  "pagesUsingSizeAdjust": true
+}
